730de9acaf6cbbdb25d4f6446b8b921d54016d75
macundkase <137147358+macundkase@users.noreply.github.com>
2025-05-22 23:30:22 +0200

    Create Settings.java

---
+++ Settings.java
@@ -0,0 +1,9 @@
+package website.monitor;
+
+import java.util.concurrent.TimeUnit;
+
+public class Settings {
+    public static final int MONITOR_INTERVAL = 30; // 30 seconds
+    public static final int NOTIFICATION_INTERVAL = 10; // 10 seconds
+    public static final TimeUnit TIME_UNIT = TimeUnit.SECONDS;
+}
